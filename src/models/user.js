812506e9c3c18a4c8d35c23b9c29c5217062dbd3
Alfarizi <arzalfarizi12@gmail.com>
2026-06-17 02:40:45 +0700

    add user data

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+export const users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "12345678",
+    name: "Seraphine",
+    role: "admin",
+  },
+  {
+    id: 2,
+    username: "myadmin",
+    password: "87654321",
+    name: "eranyang",
+    role: "admin",
+  },
+];
